bdbdd6b768b6b893b8c75c6c185cbe1045177287
Hurin <49708813+NakahodoRintaro@users.noreply.github.com>
2020-07-31 13:13:34 +0900

    Update A - Kyu in AtCoder.py

--- code/◉ M-SOLUTIONS プロコンオープン 2020/A - Kyu in AtCoder.py
+++ code/◉ M-SOLUTIONS プロコンオープン 2020/A - Kyu in AtCoder.py
@@ -1,17 +1,17 @@
-ans = input()
-if 400 <= ans <= 599:
-  print(8)
+ans = int(input())
+if 400 <= ans < 599:
+  ans = 8
 elif 600 <= ans <= 799:
-  print(7)
+  ans = 7
 elif 800 <= ans <= 999:
-  print(6)
+  ans = 6
 elif 1000 <= ans <= 1199:
-  print(5)
+  ans = 5
 elif 1200 <= ans <= 1399:
-  print(4)
+  ans = 4
 elif 1400 <= ans <= 1599:
-  print(3)
+  ans = 3
 elif 1600 <= ans <= 1799:
-  print(2)
+  ans = 2
 elif 1800 <= ans <= 1999:
-  print(1)
+  ans = 1
