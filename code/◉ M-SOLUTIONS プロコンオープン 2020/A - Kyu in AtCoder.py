1fbcf8e999a41719d7361075bb08439b32e9dbac
Hurin <49708813+NakahodoRintaro@users.noreply.github.com>
2020-07-31 13:08:03 +0900

    Create A - Kyu in AtCoder.py

---
+++ code/◉ M-SOLUTIONS プロコンオープン 2020/A - Kyu in AtCoder.py
@@ -0,0 +1,17 @@
+ans = input()
+if 400 <= ans <= 599:
+  print(8)
+elif 600 <= ans <= 799:
+  print(7)
+elif 800 <= ans <= 999:
+  print(6)
+elif 1000 <= ans <= 1199:
+  print(5)
+elif 1200 <= ans <= 1399:
+  print(4)
+elif 1400 <= ans <= 1599:
+  print(3)
+elif 1600 <= ans <= 1799:
+  print(2)
+elif 1800 <= ans <= 1999:
+  print(1)
